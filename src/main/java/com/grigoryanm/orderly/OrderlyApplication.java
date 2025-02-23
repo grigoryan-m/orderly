ff138633566a83c52cd9723ff89c4207ecad1e50
grigoryan-m <waldm4rk@gmail.com>
2025-02-23 13:10:30 +0400

    Initial commit

---
+++ src/main/java/com/grigoryanm/orderly/OrderlyApplication.java
@@ -0,0 +1,13 @@
+package com.grigoryanm.orderly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grigoryanm/orderly/OrderlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grigoryanm.orderly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
